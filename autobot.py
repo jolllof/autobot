54dcb1c5f72e92fc5b49bf9aa204cdb2e4ce64ef
Michael Hammond <michael.hammond@watershedlrs.com>
2025-01-13 09:23:06 -0500

    restructured main file based on the different categories of stocks to be analyzed

--- autobot.py
+++ autobot.py
@@ -1,6 +1,14 @@
-#TODO: finnhub basic financials could provide more details for stock's performance
 #TODO: add ADX calculations
+#TODO: automate runs
+#TODO: define a proper output notification (email/txt for now)
+#TODO: Identify if market is trending at all or something else (mean reversion)
+#TODO: add mean-reversion 
 #TODO: https://trendspider.com/markets/congress-trading/
+#TODO: finnhub basic financials could provide more details for stock's performance
+#TODO: DevOps stuff: pre-config clean up, github actions 
+
+"""Autobot evaulates trending market to find stocks based on: 
+	Run Moving AVG, ADX, ATR, RSI and Volume Based Filter and recommends buy and sell"""
 
 import os
 from strategy import *
@@ -22,6 +30,14 @@ def main():
 	start_date = datetime.today().date() - timedelta(days=730)
 	config_path = 'config.yaml'
 
+	#MANUAL LIST
+	configlists=['robinhood','quantum', 'ai', 'monitor']
+	for tickers in configlists:
+		logger.info(f"Analyzing: {tickers.upper()} Stocks For: {start_date} - {end_date}")
+		group=load_from_config(config_path, tickers)
+		db=run_analysis(group, start_date, end_date, plot=False)
+		process_data_files(tickers,end_date,db)
+
 
 	#FINNHUB Categorial Stock Search
 	stock_categories=load_from_config(config_path,'stock_categories')
@@ -31,6 +47,7 @@ def main():
 	symbols=get_all_stocks(finnhub_api_key, finnhub_base_url)
 	matches=get_stock_groups(symbols, stock_categories)
 	categoricalstocks=list(matches.keys())
+	#run_analysis(categoricalstocks, start_date, end_date)
 	
 
 	#YAHOO Finance Trending
@@ -38,23 +55,9 @@ def main():
 	yahoo_trending_url=yahoo_finance_urls['trending_url']
 	trending_tickers = fetch_trending_tickers(yahoo_trending_url)
 	trendingstocks=trending_tickers['symbol'].tolist()
-
-
-	configlists=['quantum', 'ai', 'robinhood']
-	#Run Moving AVG, ADX, ATR, RSI and Volume Based Filter
 	#run_analysis(trendingstocks, start_date, end_date)
-	#run_analysis(categoricalstocks, start_date, end_date)
-	#run_analysis(robinhoodstocks, start_date, end_date, plot=True)
-
-	for tickers in configlists:
-		logger.info(f"Analyzing: {tickers.upper()} Stocks For: {start_date} - {end_date}")
-		group=load_from_config(config_path, tickers)
-		db=run_analysis(group, start_date, end_date, plot=True)
-		process_data_files(tickers,end_date,db)
-
 
 	# send_sms_via_email(text_message)
-
 
 if __name__ == "__main__":
 	main()
