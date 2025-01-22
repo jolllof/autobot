9d82c796838bcea4bf4e27fbd3f1a238e0887f9a
Michael Hammond <michael.hammond@watershedlrs.com>
2025-01-22 11:23:57 -0500

    breaking up args_group

--- prime.py
+++ prime.py
@@ -9,6 +9,7 @@
 """Autobot evaulates trending market to find stocks based on:
 	Run Moving AVG, ADX, ATR, RSI and Volume Based Filter and recommends buy and sell"""
 
+import argparse
 import os
 from datetime import datetime, timedelta
 
@@ -26,43 +27,65 @@ os.system("clear")
 
 def main():
     logger.info("Initiating Autobot")
-
     # Configuration
     end_date = datetime.today().date()
     start_date = datetime.today().date() - timedelta(days=730)
     config_path = "config/config.yaml"
 
-    # MANUAL LIST
-    # configlists = ["robinhood", "quantum", "ai", "monitor"]
-    # for tickers in configlists:
-    #     logger.info(
-    #         f"Analyzing: {tickers.upper()} Stocks For: {start_date} - {end_date}"
-    #     )
-    #     group = load_from_config(config_path, tickers)
-    #     db = run_analysis(group, start_date, end_date, plot=False)
-    #     process_data_files(tickers, end_date, db)
+    args_group = args["args_group"]
 
-    # FINNHUB Categorial Stock Search
-    stock_categories = load_from_config(config_path, "stock_categories")
-    finnhub_creds = load_from_config(config_path, "finnhub")
-    finnhub_base_url = finnhub_creds["base_url"]
-    finnhub_api_key = os.getenv(finnhub_creds["api_key"])
-    print(finnhub_api_key)
+    if args_group.lower() == "robinhood":
+        configlists = ["robinhood"]
 
-    symbols = get_all_stocks(finnhub_api_key, finnhub_base_url)
-    matches = get_stock_groups(symbols, stock_categories)
-    categoricalstocks = list(matches.keys())
-    run_analysis(categoricalstocks, start_date, end_date)
+    elif args_group == "manual_list":
+        configlists = ["quantum", "ai", "monitor"]
 
-    # #YAHOO Finance Trending
-    # yahoo_finance_urls=load_from_config(config_path, 'yahoo_finance')
-    # yahoo_trending_url=yahoo_finance_urls['trending_url']
-    # trending_tickers = fetch_trending_tickers(yahoo_trending_url)
-    # trendingstocks=trending_tickers['symbol'].tolist()
-    # run_analysis(trendingstocks, start_date, end_date)
+    if configlists:
+        for tickers in configlists:
+            logger.info(
+                f"Analyzing: {tickers.upper()} Stocks For: {start_date} - {end_date}"
+            )
+            group = load_from_config(config_path, tickers)
+            db = run_analysis(group, start_date, end_date, plot=False)
+            process_data_files(tickers, end_date, db)
+
+    if args_group == "categorical":
+        # FINNHUB Categorial Stock Search
+        stock_categories = load_from_config(config_path, "stock_categories")
+        finnhub_creds = load_from_config(config_path, "finnhub")
+        finnhub_base_url = finnhub_creds["base_url"]
+        finnhub_api_key = os.getenv(finnhub_creds["api_key"])
+
+        symbols = get_all_stocks(finnhub_api_key, finnhub_base_url)
+        matches = get_stock_groups(symbols, stock_categories)
+        categoricalstocks = list(matches.keys())
+        run_analysis(categoricalstocks, start_date, end_date)
+
+    if args_group == "trending":
+        # YAHOO Finance Trending
+        yahoo_finance_urls = load_from_config(config_path, "yahoo_finance")
+        yahoo_trending_url = yahoo_finance_urls["trending_url"]
+        trending_tickers = fetch_trending_tickers(yahoo_trending_url)
+        trendingstocks = trending_tickers["symbol"].tolist()
+        run_analysis(trendingstocks, start_date, end_date)
 
     # send_sms_via_email(text_message)
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--set",
+        metavar="KEY=VALUE",
+        nargs="+",
+        help="""
+			Arguments need to be passed in the form of --set key1=val1 key2=val2 etc...
+			Example Arguments:
+			--set stockgroup="robinhood"
+		""",
+    )
+    args = parser.parse_args()
+    if args.set:
+        args = {v.split("=", 1)[0]: v.split("=", 1)[1] for v in list(args.set)}
+
+    main(args)
