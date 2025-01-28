59ba97701c74113c0346f0954c226c786ea1a0bb
Michael Hammond <michael.hammond@watershedlrs.com>
2025-01-28 15:01:21 -0500

    testing why its failing on github

--- strategy.py
+++ strategy.py
@@ -201,11 +201,16 @@ def run_analysis(tickers, start_date, end_date, plot=False):
             atr_above_threshold = latest_atr > atr_threshold
 
             # ADX (tug of war strength pull on both sides)
-            latest_adx = stock_data["ADX"].iloc[-1]
-            adx_is_strong = latest_adx > calc_config["strong_adx"]
-            adx_is_weak = latest_adx < calc_config["weak_adx"]
-            latest_plus_di = stock_data["+DI"].iloc[-1]
-            latest_minus_di = stock_data["-DI"].iloc[-1]
+            try:
+                latest_adx = stock_data["ADX"].iloc[-1]
+                adx_is_strong = latest_adx > calc_config["strong_adx"]
+                adx_is_weak = latest_adx < calc_config["weak_adx"]
+                latest_plus_di = stock_data["+DI"].iloc[-1]
+                latest_minus_di = stock_data["-DI"].iloc[-1]
+            except Exception as e:
+                logger.warn(f"Skipping {ticker}: ADX failed {e}")
+                print(stock_data)
+                continue
 
             # Volume Filter
             latest_volume_confirmed = stock_data["Volume_Confirmed"].iloc[-1]
